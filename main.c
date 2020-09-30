74404ea51a44eeb24d8a747636314cd014cb13b2
Avinash Pai <paiavinash200@gmail.com>
2020-09-30 12:58:14 -0700

    linked_list sorting NOT working yet.

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <linkedlist.h>
+#include <slinkedlist.h>
 
 typedef enum { INT, FLOAT, FLOAT_ARR, CHAR, STRING, STRUCT } type;
 
@@ -29,28 +29,29 @@ int main(int argc, char **argv) {
   float arr2[3] = {4.f, 5.f, 6.f};
   float arr3[3] = {7.f, 8.f, 9.f};
 
-  LL_CREATE(ll1);
-  LL_PUSH_FRONT(ll1, 3, sizeof(float), arr2, FLOAT_ARR);
-  LL_PUSH_FRONT(ll1, 3, sizeof(float), arr1, FLOAT_ARR);
+  SLL_CREATE(ll1);
+  SLL_PUSH_FRONT(ll1, 3, sizeof(float), arr2, FLOAT_ARR);
+  SLL_PUSH_FRONT(ll1, 3, sizeof(float), arr1, FLOAT_ARR);
 
-  LL_CREATE(ll2);
-  LL_PUSH_FRONT(ll2, 1, sizeof(int), &n2, INT);
-  LL_PUSH_FRONT(ll2, 1, sizeof(int), &n1, INT);
+  SLL_CREATE(ll2);
+  SLL_PUSH_FRONT(ll2, 1, sizeof(int), &n2, INT);
+  SLL_PUSH_FRONT(ll2, 1, sizeof(int), &n3, INT);
+  SLL_PUSH_FRONT(ll2, 1, sizeof(int), &n1, INT);
 
-  LL_TO_STRING(ll1, float_arr_print_fmt);
-  LL_TO_STRING(ll2, int_print_fmt);
-  list *ll2_sorted = LL_SORT(ll2, int_cmp);
+  SLL_TO_STRING(ll1, float_arr_print_fmt);
+  SLL_TO_STRING(ll2, int_print_fmt);
+  slist *ll2_sorted = SLL_SORT(ll2, int_cmp);
   printf("SORTED: \n");
-  LL_TO_STRING(*ll2_sorted, int_print_fmt);
+  SLL_TO_STRING(*ll2_sorted, int_print_fmt);
 
-  printf("ll1 size: %zu\n", ll_size(&ll1));
-  printf("ll2 size: %zu\n", ll_size(&ll2));
+  printf("ll1 size: %zu\n", sll_size(&ll1));
+  printf("ll2 size: %zu\n", sll_size(&ll2));
 
-  printf("ll1 empty? %s\n", ll_empty(&ll1) ? "true" : "false");
-  printf("ll2 empty? %s\n", ll_empty(&ll2) ? "true" : "false");
+  printf("ll1 empty? %s\n", sll_empty(&ll1) ? "true" : "false");
+  printf("ll2 empty? %s\n", sll_empty(&ll2) ? "true" : "false");
 
-  LL_CLEANUP(ll1);
-  LL_CLEANUP(ll2);
+  SLL_CLEANUP(ll1);
+  SLL_CLEANUP(ll2);
 
   return 0;
 }
